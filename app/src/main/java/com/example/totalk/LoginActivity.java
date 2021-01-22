5f9302de4c994f9e29e4b3b315bc4803eeccf782
OmaymaMajid <omaymamjd05@gmail.com>
2021-01-22 15:46:20 +0100

    Fourth Commit

--- app/src/main/java/com/example/totalk/LoginActivity.java
+++ app/src/main/java/com/example/totalk/LoginActivity.java
@@ -1,19 +1,31 @@
 package com.example.totalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
     private Button LoginButton, PhoneLoginButton;
     private EditText UserEmail, UserPassword;
     private TextView NewAccountLink, ForgotPasswordLink;
@@ -24,6 +36,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        currentUser= mAuth.getCurrentUser();
+
         InitializeFields();
 
         NewAccountLink.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +46,48 @@ public class LoginActivity extends AppCompatActivity {
                 SendUserToRegisterActivity();
             }
         });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowUserToLogin();
+            }
+        });
+    }
+
+    private void AllowUserToLogin() {
+        String email= UserEmail.getText().toString();
+        String password=UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter your email..", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter your password..", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Sign In");
+            loadingBar.setMessage("Please Wait..");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                SendUserToMainActivity();
+                                Toast.makeText(LoginActivity.this, "Logged in Successful..", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else {
+                                String message = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Error: "+message,Toast.LENGTH_LONG).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+
+        }
     }
 
     private void InitializeFields() {
@@ -41,6 +97,9 @@ public class LoginActivity extends AppCompatActivity {
         UserPassword= (EditText) findViewById(R.id.login_password);
         NewAccountLink= (TextView) findViewById(R.id.New_Account);
         ForgotPasswordLink=(TextView) findViewById(R.id.forget_password_link);
+
+        mAuth= FirebaseAuth.getInstance();
+        loadingBar= new ProgressDialog(this);
     }
 
     @Override
--- app/src/main/java/com/example/totalk/RegisterActivity.java
+++ app/src/main/java/com/example/totalk/RegisterActivity.java
@@ -1,18 +1,32 @@
 package com.example.totalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button CreateAccountButton;
     private EditText UserEmail, UserPassword;
     private TextView AlreadyHvAccountLink;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,48 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNewAccount();
+            }
+        });
+    }
+
+    private void CreateNewAccount() {
+        String email= UserEmail.getText().toString();
+        String password=UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter your email..", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter your password..", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Creating New Account");
+            loadingBar.setMessage("Please Wait..");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                SendUserToLoginActivity();
+                                Toast.makeText(RegisterActivity.this, "Account Created Successfuly..", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else {
+                                String message= task.getException().toString();
+                                Toast.makeText(RegisterActivity.this, "Error: "+message, Toast.LENGTH_LONG).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+
     }
 
     private void InitializeFields() {
@@ -35,6 +91,9 @@ public class RegisterActivity extends AppCompatActivity {
         UserEmail= (EditText) findViewById(R.id.register_email);
         UserPassword= (EditText) findViewById(R.id.register_password);
         AlreadyHvAccountLink= (TextView) findViewById(R.id.already_have_account);
+
+        mAuth= FirebaseAuth.getInstance();
+        loadingBar= new ProgressDialog(this);
     }
 
     private void SendUserToLoginActivity() {
